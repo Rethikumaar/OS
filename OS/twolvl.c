012b8b798038c86904c6885bf00b194ea93f8353
Rethikumaar <rethikumaar4574@gamil.com>
2024-01-04 13:42:33 +0530

    chganges11

--- OS/twolvl.c
+++ OS/twolvl.c
@@ -1,54 +1,93 @@
- #include<stdio.h>
+#include<stdio.h>
 #include<conio.h>
 #include<string.h>
-#include<stdlib.h>
+#include<stdlib.H>
 struct
 {
-char dname[10],fname[10][10];
+char dname[10], fname[10][10];
 int fcnt;
-}
-dir[10];
+} dir[10];
 int main()
 {
-    int i,ch,dcnt,k;
-    char f[30],d[30];
-    dcnt=0;
-    while(1)
+int i, ch, dcnt,k;
+char f[30], d[30];
+dcnt=0;
+while(1)
+{
+    printf("\n\n1.Create directory\t2.Create file\t3.Delete file");
+    printf("\n4.Search file\t Display\t6.Exit\n Enter your choice");
+    scnf("%d",&ch);
+    switch(ch)
     {
-        printf("\n\n1.Create File\t2.Delete File\t3.Search File");
-        printf("\n4.Display File\t5.Exit\nEnter your choice--");
-scanf("%d",&ch);
-switch (ch)
-{
-case 1:
-printf("\nEnter name of file directory--");
-scanf("%s",dir[dcnt].dname);
-dir[dcnt].fcnt=0;
-dcnt++;
-    break;
-case 2:
-printf("\n Enter the name of the directory--");
-scanf("%s",f);
-for(i=0;i<dcnt;i++)
-{
-if(strcmp(f,dir[i].dname[i])==0)
-{
-printf("Enter the file--");
-scanf("%s",dir[i].fname[dir[i].fcnt]);
-dir[i].fcnt++;
-printf("File created");
-}
-if(i==dcnt)
-printf("Directory %s not found",d);
-
-break;
-case 3:
-printf("enter name of the directory")
-scanf("%s",d);
-for(i=0;i<dcnt;i++)
-
-default:
-    break;
-}
+    case 1:
+        printf("\n Enter name of directory --");
+        scanf("%s",dir[dcnt].dname);
+        dir[dcnt].fcnt=0;
+        dcnt++;
+        printf("Directory created");
+        break;
+    case 2:
+        printf("\n Enter the name of the directiory--");
+        scanf("%s",d);
+        for(i=0;i<dcnt;i++)
+            if(strcmp(d,dir[i].dname)==0)
+        {
+            printf("Enter name of the file--");
+            scanf("%s",dir[i].fname[dir[i].fcnt]);
+            dir[i].fcnt++;
+            printf("File created");
+        }
+            if(i==dcnt)
+                printf("Directory %S not found",d);
+            break;
+    case 3:
+        printf("\nEnter name of the directory --");
+        scanf("%s",d);
+        for(i=0;i<dcnt;i++)
+            for(i=0;i<dcnt;i++)
+        {
+            if(strcmp(d,dir[i].dname)==0)
+            {
+                printf("Enter name of the file--");
+                scanf("%s",f);
+                for(k=0;k<dir[i].fcnt;k++)
+                {
+                    if(strcmp(f,dir[i].fname[k])==0)
+                    {
+                        printf("File %s is deleted",f);
+                        dir[i].fcnt--;
+                        strcpy(dir[i].fname[k],dir[i].fname[dir[i].fcnt]);
+                        goto jmp;
+                    }
+                }
+                printf("File %s not found",f);
+                goto jmp;
+            }
+        }
+        printf("Directory %s not found",d);
+    jmp:
+        break;
+    case 4:
+        printf("\n Enter name of the directory--");
+        scanf("%s",d);
+        for(i=0;i<dcnt;i++)
+        {
+            if(strcmp(d,dir[i].dname)==0)
+            {
+                printf("Enter the name of the file--");
+                scanf("%s",f);
+                for(k=0;k<dir[i].fcnt;k++)
+                {
+                    if(strcmp(f,dir[i].fname[k])==0)
+                    {
+                        printf("File %s is found",f);
+                        goto jmp1;
+                    }
+              
+              }
+              
+            }
+        }
     }
+}
 }
